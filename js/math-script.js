dd94af202ee9303cf7f560a171c74d41c431eb7f
moskvin73 <moskvin73av@gmail.com>
2026-06-18 21:02:00 +0300

    1

--- js/math-script.js
+++ js/math-script.js
@@ -80,6 +80,7 @@ const setTxt = (id, val) => {
     
     const globalLink = document.createElement('link');
     globalLink.rel = 'icon';
+    globalLink.type = 'image/svg+xml';
     // Эмодзи диаграммы/аналитики 📊 в формате SVG
     globalLink.href = 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📊</text></svg>';
     
