e262a54c21cb6db4ba457d38939abfe57491d9ae
moskvin73 <moskvin73av@gmail.com>
2026-06-26 21:57:43 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Глобальные конфигурационные параметры инженерного комплекса ТКЗ
 const AppConfig = {
     // Единая точка доступа к серверу Google Apps Script Web App
-    GOOGLE_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzCgkIoPTaIgTVDlnSAbgeAYPVSRyZ9PPcdx0EBhWdBqIm7K7zrZX940cpPo6FU02XtgQ/exec"
+    GOOGLE_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxlLzTe1RnRLlhjpDuofIRtYx4t0LR1-12WlGXuz21E-mucmebm-ZB59-a94ccKjn6H9A/exec"
 };
 
 // Замораживаем объект, чтобы исключить случайное изменение адреса из других скриптов
