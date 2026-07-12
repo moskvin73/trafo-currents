4a70f91f404646c02d2276089398bdc00127c2c5
moskvin73 <moskvin73av@gmail.com>
2026-07-12 19:35:47 +0300

    1

--- js/math_parser/ASTNodes.js
+++ js/math_parser/ASTNodes.js
@@ -53,7 +53,7 @@ export class NumberNode extends ASTNode {
 
   getPriority() { return OpPriority.PRIMARY; }
 
-  toString() { this.value.toString(); }
+  toString() { return this.value.toString(); }
 
   evaluate(context) { return this.value; }
 
