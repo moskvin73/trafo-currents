635528f44dfefa390777ccc28b19d808bc69b0b4
moskvin73 <moskvin73av@gmail.com>
2026-07-13 21:45:13 +0300

    1

--- js/math_parser/MathLexer.js
+++ js/math_parser/MathLexer.js
@@ -140,7 +140,7 @@ export class MathLexer {
     this.currentColumn = baseColumn;
   }*/
 
-  #advanceChar() {
+  /*#advanceChar() {
     if (this.i >= this.chars.length) return null;
     const char = this.chars[this.i];
     if (char === '\n' || char === '\r' || char === '\u2028' || char === '\u2029') {
@@ -159,7 +159,7 @@ export class MathLexer {
 
   #peekNextChar() {
     return this.i + 1 < this.chars.length ? this.chars[this.i + 1] : null;
-  }
+  }*/
 
   #readCodePointAndAdvance() {
     if (this.i >= this.source.length) return null;
@@ -209,10 +209,171 @@ export class MathLexer {
     return code;
   }
 
+  next() {
+    const src = this.source;
+    const len = src.length;
+
+    while (this.i < len) {
+      const code = src.codePointAt(this.i);
+      const charClass = code < 128 ? asciiMap[code] : C_UNKNOWN;
+
+      const startLine = this.currentLine;
+      const startColumn = this.currentColumn;
+      const startIndex = this.i;
+
+      // --- БЫСТРАЯ ASCII ДОРОЖКА ---
+      if (code < 128) {
+        switch (charClass) {
+          case C_SPACE: {
+            this.#readCodePointAndAdvance(); // Просто поглощаем пробел
+            continue;
+          }
+
+          case C_OPERATOR: {
+            // Комментарии //
+            if (code === 47 && src.charCodeAt(this.i + 1) === 47) {
+              this.#readCodePointAndAdvance(); this.#readCodePointAndAdvance();
+              while (this.i < len) {
+                const next = src.charCodeAt(this.i);
+                if (next === 10 || next === 13 || next === 8232 || nextCode === 8233 || next === 133 || next === 12) break;
+                this.#readCodePointAndAdvance();
+              }
+              continue;
+            }
+            // Степень **
+            if (code === 42 && src.charCodeAt(this.i + 1) === 42) {
+              this.#readCodePointAndAdvance(); this.#readCodePointAndAdvance();
+              return new Token(TokenType.POW, '^', new SourceLocation(startLine, startColumn, startIndex));
+            }
+
+            this.#readCodePointAndAdvance();
+            let type;
+            if (code === 43) type = TokenType.PLUS;
+            else if (code === 45) type = TokenType.MINUS;
+            else if (code === 47) type = TokenType.DIV;
+            else if (code === 61) type = TokenType.ASSIGN;
+            else if (code === 40) type = TokenType.LPAREN;
+            else if (code === 41) type = TokenType.RPAREN;
+            else if (code === 59) type = TokenType.SEMICOLON;
+            else if (code === 36) type = TokenType.SILENT;
+            else if (code === 44) type = TokenType.COMMA;
+            else if (code === 94) type = TokenType.POW;
+            else if (code === 42) type = TokenType.MUL;
+
+            return new Token(type, String.fromCharCode(code), new SourceLocation(startLine, startColumn, startIndex));
+          }
+
+          case C_QUOTE: {
+            const quote = code;
+            this.#readCodePointAndAdvance();
+            const textStart = this.i;
+            while (this.i < len && src.charCodeAt(this.i) !== quote) {
+              this.#readCodePointAndAdvance();
+            }
+            const textValue = src.slice(textStart, this.i);
+            const startLoc = new SourceLocation(startLine, startColumn, startIndex);
+            if (this.i >= len) {
+              this.errors.push(new CompilerError(`Незакрытая текстовая строка`, startLoc));
+              return new Token(TokenType.TEXT_BLOCK, textValue, startLoc);
+            }
+            this.#readCodePointAndAdvance();
+            return new Token(TokenType.TEXT_BLOCK, textValue, startLoc);
+          }
+
+          case C_PERCENT: { // %pi, %e
+            this.#readCodePointAndAdvance();
+            const constStart = this.i;
+            while (this.i < len) {
+              const next = src.charCodeAt(this.i);
+              if ((next >= 65 && next <= 90) || (next >= 97 && next <= 122)) this.#readCodePointAndAdvance();
+              else break;
+            }
+            const constName = '%' + src.slice(constStart, this.i);
+            const matchedType = constantMap[constName];
+            const startLoc = new SourceLocation(startLine, startColumn, startIndex);
+            if (matchedType) return { type: matchedType, value: constName, loc: startLoc };
+            this.errors.push(new CompilerError(`Неизвестная математическая константа "${constName}"`, startLoc));
+            continue;
+          }
+
+          case C_DIGIT: { // Числа + Экспонента
+            const numStart = this.i;
+            let dotCount = 0;
+            while (this.i < len) {
+              const next = src.charCodeAt(this.i);
+              if (next === 46) { dotCount++; this.#readCodePointAndAdvance(); }
+              else if (next >= 48 && next <= 57) this.#readCodePointAndAdvance();
+              else break;
+            }
+            // Научная нотация e/E
+            if (this.i < len) {
+              const next = src.charCodeAt(this.i);
+              if (next === 101 || next === 69) {
+                let look = this.i + 1, hasSign = false;
+                if (look < len && (src.charCodeAt(look) === 43 || src.charCodeAt(look) === 45)) { look++; hasSign = true; }
+                if (look < len && src.charCodeAt(look) >= 48 && src.charCodeAt(look) <= 57) {
+                  this.#readCodePointAndAdvance(); if (hasSign) this.#readCodePointAndAdvance();
+                  while (this.i < len && src.charCodeAt(this.i) >= 48 && src.charCodeAt(this.i) <= 57) this.#readCodePointAndAdvance();
+                }
+              }
+            }
+            const numStr = src.slice(numStart, this.i);
+            const startLoc = new SourceLocation(startLine, startColumn, startIndex);
+            if (dotCount > 1) this.errors.push(new CompilerError(`Неверный формат числа "${numStr}"`, startLoc));
+            const parsedVal = parseFloat(numStr) || 0;
+            if (this.i < len && src.charCodeAt(this.i) === 105) { this.#readCodePointAndAdvance(); return new Token(TokenType.COMPLEX_NUMBER, parsedVal, startLoc); }
+            return new Token(TokenType.NUMBER, parsedVal, startLoc);
+          }
+
+          case C_ALPHA: { // Идентификаторы (Латиница)
+            const idStart = this.i;
+            this.#readCodePointAndAdvance();
+            while (this.i < len) {
+              const next = src.charCodeAt(this.i);
+              if ((next >= 65 && next <= 90) || (next >= 97 && next <= 122) || (next >= 48 && next <= 57) || next === 95) {
+                this.#readCodePointAndAdvance();
+              } else if (next > 127) { // Переход в Юникод посреди слова
+                if (isUnicodeLetter(src.codePointAt(this.i))) this.#readCodePointAndAdvance();
+                else break;
+              } else break;
+            }
+            return new Token(TokenType.VARIABLE, src.slice(idStart, this.i), new SourceLocation(startLine, startColumn, startIndex));
+          }
+        }
+      } else {
+        // --- ТОЧНАЯ ЮНИКОД ДОРОЖКА (code >= 128) ---
+        if (isUnicodeSpace(code)) {
+          this.#readCodePointAndAdvance();
+          continue;
+        }
+
+        if (isUnicodeLetter(code)) {
+          const idStart = this.i;
+          this.#readCodePointAndAdvance();
+          while (this.i < len) {
+            const nextCp = src.codePointAt(this.i);
+            // Буквы или цифры (ASCII / Юникод) внутри названия переменной
+            if ((nextCp < 128 && (asciiMap[nextCp] === C_ALPHA || asciiMap[nextCp] === C_DIGIT)) || isUnicodeLetter(nextCp)) {
+              this.#readCodePointAndAdvance();
+            } else break;
+          }
+          return new Token(TokenType.VARIABLE, src.slice(idStart, this.i), new SourceLocation(startLine, startColumn, startIndex));
+        }
+
+        // Безопасный отлов сломанных/неизвестных символов
+        const currentPos = this.i;
+        this.#readCodePointAndAdvance(); // Сдвинет на 1 или 2 в зависимости от валидности суррогата
+        const badChar = src.slice(currentPos, this.i);
+        this.errors.push(new CompilerError(`Неизвестный символ "${badChar}"`, new SourceLocation(startLine, startColumn, startIndex)));
+      }
+    }
+
+    return new Token(TokenType.EOF, 'EOF', new SourceLocation(this.currentLine, this.currentColumn, this.i));
+  }  
   /**
    * Считывает и возвращает СЛЕДУЮЩИЙ единственный токен из потока (LL(1))
    */
-  next() {
+  /*next() {
     while (this.i < this.chars.length) {
       let char = this.chars[this.i];
       let startLoc = new SourceLocation(this.currentLine, this.currentColumn, this.i);
@@ -326,5 +487,5 @@ export class MathLexer {
     }
 
     return new Token(TokenType.EOF, 'EOF', new SourceLocation(this.currentLine, this.currentColumn, this.i));
-  }
+  }*/
 }
