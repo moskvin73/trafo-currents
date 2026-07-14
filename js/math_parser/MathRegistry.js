e0478fde296180f67537b391816ca90efc052085
moskvin73 <moskvin73av@gmail.com>
2026-07-14 09:03:11 +0300

    1

--- js/math_parser/MathRegistry.js
+++ js/math_parser/MathRegistry.js
@@ -99,12 +99,13 @@ export const MathRegistry = {
   /**
    * Диспетчер сигнатур с алгоритмом автоматического скоринга и приведения типов
    */
-  execute(name, args, loc, context = {}) {
+  /*execute(name, args, loc, context = {}) {
     const overloads = COMPILER_REGISTRY.get(name);
     if (!overloads) {
       throw new Error(`[Semantic Error]: Функция "${name}" не поддерживается вычислительным ядром на ${loc}`);
-    }
-
+    }*/
+execute(overloads, args, loc, context = {}) {
+  
     const arity = args.length;
     let bestOverload = null;
     let finalArgs = null;
@@ -211,68 +212,3 @@ export const MathRegistry = {
     }
   }
 };
-
-//const dispatcher = new SemanticDispatcher();
-
-//export const MathRegistry = {
-  /**
-   * Главный диспетчер вызова аналитических функций
-   * @param {string} name - Имя функции
-   * @param {MathType[]} args - Массив уже ВЫЧИСЛЕННЫХ объектов MathType
-   * @param {SourceLocation} loc - Координаты для вывода ошибок
-   */
-/*  execute(name, args, loc) {
-    
-    // ==========================================
-    // 1. ОПРЕДЕЛЕНИЕ СИГНАТУРЫ ПО КОЛИЧЕСТВУ АРГУМЕНТОВ
-    // ==========================================
-
-    // --- Одноаргументные функции (sin, cos, sqrt, log...) ---
-    if (args.length === 1) {
-      const arg = args[0];
-
-      switch (name) {
-        case 'sqrt': return arg.sqrt();
-        case 'lg': return arg.log10();
-        case 'ln': return arg.log();
-        case 'exp':  return arg.exp();
-
-        case 'sin':  return arg.sin();
-        case 'cos':  return arg.cos();
-        case 'tan':  return arg.tan();
-        case 'sinh':  return arg.sin();
-        case 'cosh':  return arg.cos();
-        case 'tanh':  return arg.tan();
-
-        case 'arcsin':  return arg.arcsin();
-        case 'arccos':  return arg.arccos();
-        case 'arctan':  return arg.arctan();
-        case 'arctanh':  return arg.arctanh();
-        
-        case 'arcsinh':  return arg.arcsinh();
-        case 'arccosh':  return arg.arccosh();        
-        default:
-          throw new Error(`[Semantic Error]: Функция "${name}" с одним аргументом не поддерживается на ${loc}`);
-      }
-    }
-
-    // --- Двухаргументные функции (pow, solve...) ---
-    if (args.length === 2) {
-      const [arg1, arg2] = args;
-
-      const { l, r } = dispatcher.promoteTypes(arg1, arg2);
-      switch (name) {
-        case 'pow': return l.accuratePow(r);
-        case 'log': return l.logBase(r);
-        case 'sqrt': return l.sqrt(r);
-        default:
-          throw new Error(`[Semantic Error]: Функция "${name}" с двумя аргументами не поддерживается на ${loc}`);
-      }
-    }
-
-    // --- Задел на будущее для Линейной Алгебры (СЛАУ / Функции с переменным числом аргументов) ---
-    // if (name === 'solve') { return LinearAlgebra.solve(args); }
-
-    throw new Error(`[Semantic Error]: Неверное количество аргументов (${args.length}) для функции "${name}" на ${loc}`);
-  }
-};*/
