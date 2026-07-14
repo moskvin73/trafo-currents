3631aef7de32f5a08df61bf430d39192e1a440f8
moskvin73 <moskvin73av@gmail.com>
2026-07-14 09:47:10 +0300

    1

--- js/math_parser/MathParser.js
+++ js/math_parser/MathParser.js
@@ -498,7 +498,7 @@ export class MathParser {
   #callFuncORVar() {
       const token_loc = this.location;
       const id_name = this.lexer.stringValue();      
-      const id = this.context.getSymbolByName(this.lexer.stringValue());
+      const id = this.context.getSymbolByName(id_name);
       let is_error = false;
       if (id === null) {
         this.#error(`Неопределённый идентификатор "${id_name}"`, token_loc);
