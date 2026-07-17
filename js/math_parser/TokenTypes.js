56c23b1e2ff47a2cb7eaf43e5c13e990e0adb1ab
moskvin73 <moskvin73av@gmail.com>
2026-07-17 07:02:26 +0300

    1

--- js/math_parser/TokenTypes.js
+++ js/math_parser/TokenTypes.js
@@ -1,27 +1,3 @@
-/*export const TokenType = Object.freeze({
-  EOF: 0,
-  NUMBER: 1,         // Вещественное число
-  COMPLEX_NUMBER: 2, // Комплексное число (1i, 4i)
-  PLUS: 3,           // +
-  MINUS: 4,          // -
-  MUL: 5,            // *
-  DIV: 6,            // /
-  POW: 7,            // ^ или **
-  ASSIGN: 8,         // = 
-  LPAREN: 9,         // (
-  PARENR: 10,        // )
-  VARIABLE: 11,      // Переменные
-  TEXT_BLOCK: 12,    // "строка"
-  COMMENT: 13,        // // комментарий
-  SEMICOLON: 14, // ;
-  SILENT: 15,     // $ 
-  COMMA: 16,        // , 
-  MATH_PI: 17,   // %pi
-  MATH_E: 18,     // %e
-  MATH_PHI: 19, // %phi
-  MATH_INF: 20, // %inf 
-  MATH_NAN: 21,  // %nan  
-});*/
 // Просто пишем список названий по порядку
 const tokenNames = [
   'EOF',
@@ -47,6 +23,13 @@ const tokenNames = [
   'MATH_INF',
   'MATH_NAN',
   'RW_PRINT',
+  'RW_TRUE',
+  'RW_FALSE',
+  'RW_PLOT_INIT',
+  'RW_PLOT_CONFIG',
+  'RW_PLOT_LAYER',
+  'RW_PLOT_VECTOR',
+  'RW_PLOT_CHORD',
 ];
 
 // Создаем пустой объект перечисления
