f515d47c12e9589f6e6d12f9a1b7ec2531c2559d
moskvin73 <moskvin73av@gmail.com>
2026-07-17 14:52:48 +0300

    1

--- js/math_parser/DiagramNodes.js
+++ js/math_parser/DiagramNodes.js
@@ -1,12 +1,31 @@
 import ASTNode,  from './ASTNodes.js';
 import ASTNode,  from '../views/DiagramDescriptor.js';
-
+import { createFloatingWindowDOM },  from '../views/util.js';
 
 export class PlotInitNode extends ASTNode {
-  constructor(name, mode, loc) {
-    super(loc); 
-    this.name = name; // Имя функции (строка)
-    this.args = args; // Массив дочерних узлов ASTNode
+  constructor(name, mode, viewType, loc) {
+    super(loc);
+    this.diagramId = diagramId;
+    this.mode = mode;
+    this.viewType = viewType;
   }
 
+  internal_evaluate(context) { 
+     // Создаем экземпляр нашего нового класса-описателя
+    const descriptor = new DiagramDescriptor(this.mode, this.viewType);
+    
+    // Регистрируем в SymbolTableContext
+    const id = context.scope_context.acquireId(this.diagramId);
+    const sym = context.scope_context.getSymbolById(id);
+    sym.value = descriptor;
+
+    // Если режим window — сразу генерируем плавающее окно
+    if (this.viewType === "window") {
+        descriptor.containerElement = createFloatingWindowDOM(this.diagramId);
+        // Инициализируем графический движок VectorDiagram внутри созданного окна
+        descriptor.instance = new VectorDiagram(descriptor.containerElement, descriptor.data);
+    }
+
+    return descriptor;     
+  }
 }
