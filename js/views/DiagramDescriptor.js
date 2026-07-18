e28fcc99e194e0e8e68f3c592b9df9d7d108e107
moskvin73 <moskvin73av@gmail.com>
2026-07-18 06:48:54 +0300

    1

--- js/views/DiagramDescriptor.js
+++ js/views/DiagramDescriptor.js
@@ -33,7 +33,7 @@ export default class DiagramDescriptor {
             throw new TypeError("Инициализация невозможна: containerElement должен быть валидным DOM-элементом.");
         }
 
-        this.instance = new VectorDiagram(contentDiv, descriptor.data);
+        this.instance = new VectorDiagram(contentDiv, data);
         this.#containerElement = contentDiv;
     }
 
