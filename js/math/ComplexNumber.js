a38213fd1b4b50683816e7e8245bbc16c35b5203
moskvin73 <moskvin73av@gmail.com>
2026-07-25 17:22:33 +0300

    1

--- js/math/ComplexNumber.js
+++ js/math/ComplexNumber.js
@@ -606,7 +606,7 @@ export default class ComplexNumber extends MathType {
   }
 
   not_eq(other) {
-    const right = ComplexNumber.from(other).#value;
+    const right = ComplexNumber.from(other);
     return this.#real !== right.real || this.#imaginary !== right.imaginary;  
   }
 
