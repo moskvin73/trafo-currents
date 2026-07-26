d74e075010538c5a52e88280353c2d3d0199970d
moskvin73 <moskvin73av@gmail.com>
2026-07-26 19:32:18 +0300

    1

--- js/math_parser/MathParser.js
+++ js/math_parser/MathParser.js
@@ -395,7 +395,7 @@ export class MathParser {
         return;
       case TokenType.RW_DO:
         this.#parseDoWhere(code, f_out);
-        break;
+        break; // Требуем ; или $
       case TokenType.SEMICOLON:
       case TokenType.SILENT:
         // Проопускаем пустые ;;; $$$$ разделители операторов
