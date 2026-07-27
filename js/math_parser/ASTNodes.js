43522097d48131f34caefd15d883099a8de66611
moskvin73 <moskvin73av@gmail.com>
2026-07-27 20:46:24 +0300

    1

--- js/math_parser/ASTNodes.js
+++ js/math_parser/ASTNodes.js
@@ -448,7 +448,7 @@ export class CastOpNode extends MathNode {
     const type = typeof valueToCast;
     const sourceType = type === 'object' && valueToCast !== null ? valueToCast.constructor : type;
 
-    const kkk =  CAST_TABLE[sourceType];
+    const kkk =  CAST_TABLE.get(sourceType);
     const castFunction = CAST_TABLE[sourceType]?.[this.targetType];
     if (castFunction) {
       return castFunction(valueToCast);
