c3480958560359f311b412897a8525e26401daf8
moskvin73 <moskvin73av@gmail.com>
2026-07-28 01:50:11 +0300

    1

--- js/math_parser/MathRegistry.js
+++ js/math_parser/MathRegistry.js
@@ -354,7 +354,8 @@ function executeMatrixOperationUniversal(matrix, extraArgs, overloads) {
 let roundRules = [];
 roundRules =
 [
-  { types: [RealNumber], callType: 'custom', execute: ([x]) => new RealNumber(Math.round(x.value)) },
+  { types: [RealNumber], callType: 'custom', execute: ([x]) => 
+                                      new RealNumber(Math.round(x.value)) },
   { types: [RealNumber, RealNumber], callType: 'custom', execute: 
                                       ([x, y]) => new RealNumber(roundNumber(x.value, y.value)) },
   { types: [ComplexNumber], callType: 'custom', execute: ([x]) => { 
