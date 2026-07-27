51f9d860b39bbb4b7e2952c4e5af3c61e5427752
moskvin73 <moskvin73av@gmail.com>
2026-07-27 23:27:52 +0300

    1

--- js/math_parser/MathRegistry.js
+++ js/math_parser/MathRegistry.js
@@ -52,17 +52,17 @@ export const COMPILER_REGISTRY = new Map([
   ]],
 
   ['round', [
-    { types: [RealNumber], callType: 'custom', execute: ([x]) => new RealNumber(roundNumber(x.value)) },
+    { types: [RealNumber], callType: 'custom', execute: ([x]) => new RealNumber(Math.round(x.value)) },
     { types: [RealNumber, RealNumber], callType: 'custom', execute: 
                                         ([x, y]) => new RealNumber(roundNumber(x.value, y.value)) },
     { types: [ComplexNumber], callType: 'custom', execute: ([x]) => { 
-          const r = roundNumber(x.real);
-          const i = roundNumber(x.imaginary);
-          new ComplexNumber(r, i) }},
+          const r = Math.round(x.real);
+          const i = Math.round(x.imaginary);
+          return new ComplexNumber(r, i) }},
     { types: [ComplexNumber, RealNumber], callType: 'custom', execute: ([x, y]) => { 
           const r = roundNumber(x.real, y);
           const i = roundNumber(x.imaginary, y);
-          new ComplexNumber(r, i) }},
+          return new ComplexNumber(r, i) }},
     //{ types: [Matrix], callType: 'custom', execute: ([x]) => x.map(abs) },
   ]],
 
