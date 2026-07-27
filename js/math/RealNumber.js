2ca54f8a8045a82a1e86791e8c518c534fc542fb
moskvin73 <moskvin73av@gmail.com>
2026-07-27 12:27:42 +0300

    1

--- js/math/RealNumber.js
+++ js/math/RealNumber.js
@@ -32,6 +32,10 @@ export default class RealNumber extends MathType {
     return this.#value;
   }
 
+  get conjugate() {
+    return new RealNumber(this.#value);
+  }
+
   // ==========================================
   // ВСПОМОГАТЕЛЬНЫЕ МЕТОДЫ ПРИВЕДЕНИЯ ТИПОВ
   // ==========================================
