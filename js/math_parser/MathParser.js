5ebabfa8276545518ae14f338c3087f41e907d98
moskvin73 <moskvin73av@gmail.com>
2026-07-28 15:44:51 +0300

    1

--- js/math_parser/MathParser.js
+++ js/math_parser/MathParser.js
@@ -1112,7 +1112,7 @@ export class MathParser {
 
   #parseExpression() {
     // Сбрасываем массив идентификаторов
-    this.#listUndefinedIdentifiers = idList = Object.create(null); 
+    this.#listUndefinedIdentifiers = Object.create(null); 
     const result = this.#parseAssignment();
     while (MathParser.Expr_FIRST.has(this.c_token))
     {
