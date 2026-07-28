2487c0b1586d25eb26c2bf7e97307dd8fa16b5c0
moskvin73 <moskvin73av@gmail.com>
2026-07-28 14:36:34 +0300

    1

--- js/math_parser/MathParser.js
+++ js/math_parser/MathParser.js
@@ -1548,15 +1548,8 @@ export class MathParser {
           }
          }
 
-         case TokenType.VARIABLE: {
-            const ret = this.#callFuncORVar();
-            // Проверяем есть ли такой идентификатор
-            const id = this.context.getIdByName(name);
-            if (!id) {
-              this.#listUndefinedIdentifiers[name] = ret;
-            }
-            return ret;
-         }
+         case TokenType.VARIABLE: 
+            return this.#callFuncORVar();
          default:
           this.#error(`Ожидался операнд "${this.lexer.stringValue()}"`, token_loc);
           while (true)
