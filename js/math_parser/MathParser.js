cabdab4827617fcd98679a00a9e15c5b84ba1659
moskvin73 <moskvin73av@gmail.com>
2026-07-29 22:03:36 +0300

    1

--- js/math_parser/MathParser.js
+++ js/math_parser/MathParser.js
@@ -1061,7 +1061,7 @@ export class MathParser {
             return new DefineVarableCodeNode(funcId, statements, 0, localCount, token_loc);
           else {
             const sym = this.context.getParseSymbolById(funcId);
-            sym.value = new new VarableCode(statements, 0, localsCount, null);
+            sym.value = new new VarableCode(statements, 0, localCount, null);
           }
         }
         return null;
@@ -1108,7 +1108,7 @@ export class MathParser {
               return new DefineVarableCodeNode(funcId, statements, params.length, localCount, token_loc);
             else {
               const sym = this.context.getParseSymbolById(funcId);
-              sym.value = new new VarableCode(statements, params.length, localsCount, null);
+              sym.value = new new VarableCode(statements, params.length, localCount, null);
             }
           }
           return null;
