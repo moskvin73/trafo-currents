44ad0382a5d92348fe9665043f9e63ae0e17f901
moskvin73 <moskvin73av@gmail.com>
2026-07-29 23:59:07 +0300

    1

--- js/math_parser/MathParser.js
+++ js/math_parser/MathParser.js
@@ -1014,7 +1014,7 @@ export class MathParser {
             jump.node.len_code = switchEndIndex - jump.index - 1;
           }
           else if (jump.node.len_code.type === MathParser.ALLOW_CONTINUE) {
-            jump.node.len_code = -(switchEndIndex - jump.index + (expCond ? 1 : 0));
+            jump.node.len_code = -(jump.index + (expCond ? 2 : 1));
           }
         }
       }
