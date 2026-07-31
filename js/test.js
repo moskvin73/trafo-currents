2ab9543fc53d60e2c395b389fe9d6cb9acbbbf93
moskvin73 <moskvin73av@gmail.com>
2026-07-31 23:45:39 +0300

    1

--- js/test.js
+++ js/test.js
@@ -37,9 +37,10 @@ export function test(data) {
     const loc = new IndependentSourceLocation(loc_data);
     const nn = new AST.NumberNode(new ComplexNumber(1, 1), loc);
     const node = new AST.AddNode(new AST.UnaryOpNodeMinus(nn, loc), nn, loc);
+    const p_node = new AST.PrintNode(["XXX", node]);
 
     //const savedState = context.serializeGlobalContext();
-    const jsonResult = JSON.stringify(node);
+    const jsonResult = JSON.stringify(p_node);
     console.log(jsonResult);
 
     const rawData = JSON.parse(jsonResult);
