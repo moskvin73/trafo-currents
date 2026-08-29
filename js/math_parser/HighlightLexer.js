eeaa91119f64c6e52b3114e4d55a71a0a1cd4088
moskvin73 <moskvin73av@gmail.com>
2026-08-29 12:37:08 +0300

    1

--- js/math_parser/HighlightLexer.js
+++ js/math_parser/HighlightLexer.js
@@ -12,55 +12,55 @@ export function htmlEscape(text) {
 
 export function HighlightLerxer(text) {
     const lexer = new MathLexer(text, [], 1, true);
-    let htmltext = '';
+    const chunks = []; 
     let token = lexer.next();
     while (token !== TokenType.EOF) {
         if (token >= TokenType.FIRST_RESERVED_CHARACTERS && token <= TokenType.LAST_RESERVED_CHARACTERS)
-            htmltext += lexer.stringValue();
+            chunks.push(lexer.stringValue());
         else if (token >= TokenType.FIRST_RESERVED_CONSTANTS && token <= TokenType.LAST_RESERVED_CONSTANTS)
-            htmltext += `<span class="token-rw-constants">${lexer.source.slice(lexer.tokenStart, lexer.tokenEnd)}</span>`;
+            chunks.push(`<span class="token-rw-constants">${lexer.source.slice(lexer.tokenStart, lexer.tokenEnd)}</span>`);
         else if (token >= TokenType.FIRST_RESERVED_WORDS && token <= TokenType.LAST_RESERVED_WORDS)
-            htmltext += `<span class="token-rw-words">${lexer.stringValue()}</span>`;
+            chunks.push(`<span class="token-rw-words">${lexer.stringValue()}</span>`);
         else switch (token) {
             case TokenType.COMMENT:
-                htmltext += `<span class="token-comment">${htmlEscape(lexer.stringValue())}</span>`;
+                chunks.push(`<span class="token-comment">${htmlEscape(lexer.stringValue())}</span>`);
                 break;
             case TokenType.SPACES:
-                htmltext += lexer.stringValue();
+                chunks.push(lexer.stringValue());
                 break;
             case TokenType.ERROR:
-                htmltext += `<span class="token-error-code">${htmlEscape(lexer.stringValue())}</span>`;
+                chunks.push(htmltext += `<span class="token-error-code">${htmlEscape(lexer.stringValue())}</span>`);
                 break;
             case TokenType.ERROR_STR:
-                htmltext += `<span class="token-error-string">${htmlEscape(lexer.source.slice(lexer.tokenStart - 1, lexer.tokenEnd))}</span>`;
+                chunks.push(`<span class="token-error-string">${htmlEscape(lexer.source.slice(lexer.tokenStart - 1, lexer.tokenEnd))}</span>`);
                 break;
             case TokenType.TEXT_BLOCK:
-                htmltext += `<span class="token-string">${htmlEscape(lexer.source.slice(lexer.tokenStart - 1, lexer.tokenEnd + 1))}</span>`;
+                chunks.push(`<span class="token-string">${htmlEscape(lexer.source.slice(lexer.tokenStart - 1, lexer.tokenEnd + 1))}</span>`);
                 break;
             case TokenType.NUMBER:
-                htmltext += `<span class="token-number">${lexer.stringValue()}</span>`;
+                chunks.push(`<span class="token-number">${lexer.stringValue()}</span>`);
                 break;
             case TokenType.COMPLEX_NUMBER:
-                htmltext += `<span class="token-complex-number">${lexer.stringValue()}</span>`;
+                chunks.push(`<span class="token-complex-number">${lexer.stringValue()}</span>`);
                 break;
             case TokenType.VARIABLE:
-                htmltext += `<span class="token-varable">${lexer.stringValue()}</span>`;
+                chunks.push(`<span class="token-varable">${lexer.stringValue()}</span>`);
                 break;
             case TokenType.LT:       // <
-                htmltext += '&lt;';
+                chunks.push('&lt;');
                 break;
             case TokenType.GT:       // >
-                htmltext += '&gt;';
+                chunks.push('&gt;');
                 break;
             case TokenType.LTE:      // <=
-                htmltext += '&lt;=';
+                chunks.push('&lt;=');
                 break;
             case TokenType.GTE:      // >=
-                htmltext += '&gt;=';
+                chunks.push('&gt;=');
                 break;
             default: throw new Error(`[HighlightLerxer]: неизвестный токен ${token}`);
         }
         token = lexer.next();
     }
-    return htmltext;
+    return chunks.join(''); 
 }
