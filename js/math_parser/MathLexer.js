3145cd041d77b26c6eae37b4e946c14518661e14
moskvin73 <moskvin73av@gmail.com>
2026-08-29 12:20:10 +0300

    1

--- js/math_parser/MathLexer.js
+++ js/math_parser/MathLexer.js
@@ -287,6 +287,15 @@ export class MathLexer {
           case C_SPACE: {
             this.#readCodePointAndAdvance();
             if (this.include_trivia) {
+               while (true)
+               {
+                const code = src.codePointAt(this.i);
+                const isSpace = code < 128 
+                    ? asciiMap[code] === C_SPACE 
+                    : isUnicodeSpace(code);
+                if (!isSpace) break;
+                this.#readCodePointAndAdvance();
+              }
               this.tokenStart = startIdx;
               this.tokenEnd = this.i;
               this.tokenStartLine = startLine;
