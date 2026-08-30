2f3fe144e5d8c21cf8f7673efdc4b20592d6cbdd
moskvin73 <moskvin73av@gmail.com>
2026-08-30 15:12:56 +0300

    1

--- js/math_parser/HighlightLexer.js
+++ js/math_parser/HighlightLexer.js
@@ -18,7 +18,7 @@ export function HighlightLerxer(text) {
     let token = lexer.next();
     while (token !== TokenType.EOF) {
         if (token >= TokenType.FIRST_RESERVED_CHARACTERS && token <= TokenType.LAST_RESERVED_CHARACTERS)
-            chunks.push(lexer.stringValue());
+            chunks.push(<span class="token-rw-characters">${lexer.stringValue()}</span>`);
         else if (token >= TokenType.FIRST_RESERVED_CONSTANTS && token <= TokenType.LAST_RESERVED_CONSTANTS)
             chunks.push(`<span class="token-rw-constants">${lexer.source.slice(lexer.tokenStart, lexer.tokenEnd)}</span>`);
         else if (token >= TokenType.FIRST_RESERVED_WORDS && token <= TokenType.LAST_RESERVED_WORDS)
