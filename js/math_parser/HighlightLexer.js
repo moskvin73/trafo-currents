84fc8775e5dcbbac5733a491cd0c52116f50ddf2
moskvin73 <moskvin73av@gmail.com>
2026-09-01 20:00:30 +0300

    1

--- js/math_parser/HighlightLexer.js
+++ js/math_parser/HighlightLexer.js
@@ -16,14 +16,14 @@ export function HighlightLerxer(text) {
     const bracketStack = [];
     let bracketIdCounter = 0; 
     let token = lexer.next();
-    let tokenDetails =  TokenDetails[token].hint  || '';
+    let hint =  TokenDetails[token].hint  || '';
     while (token !== TokenType.EOF) {
         if (token >= TokenType.FIRST_RESERVED_CHARACTERS && token <= TokenType.LAST_RESERVED_CHARACTERS)
-            chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${tokenDetails}">${lexer.stringValue()}</span>`});
+            chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${hint}">${lexer.stringValue()}</span>`});
         else if (token >= TokenType.FIRST_RESERVED_CONSTANTS && token <= TokenType.LAST_RESERVED_CONSTANTS)
-            chunks.push({ type: 'TEXT', html: `<span class="token-rw-constants" data-title="${tokenDetails}">${lexer.source.slice(lexer.tokenStart, lexer.tokenEnd)}</span>`});
+            chunks.push({ type: 'TEXT', html: `<span class="token-rw-constants" data-title="${hint}">${lexer.source.slice(lexer.tokenStart, lexer.tokenEnd)}</span>`});
         else if (token >= TokenType.FIRST_RESERVED_WORDS && token <= TokenType.LAST_RESERVED_WORDS)
-            chunks.push({ type: 'TEXT', html: `<span class="token-rw-words" data-title="${tokenDetails}">${lexer.stringValue()}</span>`});
+            chunks.push({ type: 'TEXT', html: `<span class="token-rw-words" data-title="${hint}">${lexer.stringValue()}</span>`});
         else switch (token) {
                 // --- Обработка скобок ---
                 case TokenType.LPAREN:
@@ -32,7 +32,7 @@ export function HighlightLerxer(text) {
                     const id = bracketIdCounter++;
                     // Запоминаем тип скобки, её ID и индекс в массиве chunks, чтобы потом можно было пометить ошибку
                     bracketStack.push({ type: token, id: id, chunkIndex: chunks.length }); 
-                    chunks.push({ type: 'TEXT', html: `<span class="token-bracket" data-title="${tokenDetails}" data-bracket-id="${id}" data-index="${lexer.tokenStart}">${lexer.stringValue()}</span>`});
+                    chunks.push({ type: 'TEXT', html: `<span class="token-bracket" data-title="${hint}" data-bracket-id="${id}" data-index="${lexer.tokenStart}">${lexer.stringValue()}</span>`});
                     break;
                 }
                 case TokenType.RPAREN:
@@ -54,17 +54,17 @@ export function HighlightLerxer(text) {
                         }
                     }
                     if (pairId !== null) {
-                        chunks.push({ type: 'TEXT', html: `<span class="token-bracket" data-title="${tokenDetails}" data-bracket-id="${pairId}" data-index="${lexer.tokenStart}">${lexer.stringValue()}</span>`});
+                        chunks.push({ type: 'TEXT', html: `<span class="token-bracket" data-title="${hint}" data-bracket-id="${pairId}" data-index="${lexer.tokenStart}">${lexer.stringValue()}</span>`});
                     } else {
                         // Ошибка: закрывающая скобка не имеет пары
-                        chunks.push({ type: 'TEXT', html: `<span class="token-bracket data-title="${tokenDetails}" token-bracket-error" data-index="${lexer.tokenStart}">${lexer.stringValue()}</span>`});
+                        chunks.push({ type: 'TEXT', html: `<span class="token-bracket data-title="${hint}" token-bracket-error" data-index="${lexer.tokenStart}">${lexer.stringValue()}</span>`});
                     }                    
                     break;
                 }
                 // --- Конец обработки скобок ---
 
             case TokenType.COMMENT:
-                chunks.push({ type: 'TEXT', html: `<span class="token-comment" data-title="${tokenDetails}">${htmlEscape(lexer.stringValue())}</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-comment" data-title="${hint}">${htmlEscape(lexer.stringValue())}</span>`});
                 break;
             case TokenType.SPACES:
                 chunks.push({ type: 'TEXT', html: lexer.stringValue()});
@@ -73,39 +73,39 @@ export function HighlightLerxer(text) {
                 chunks.push({ type: 'NL', html: '<br>' });
                 break;
             case TokenType.ERROR:
-                chunks.push({ type: 'TEXT', html: `<span class="token-error-code" data-title="${tokenDetails}">${htmlEscape(lexer.stringValue())}</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-error-code" data-title="${hint}">${htmlEscape(lexer.stringValue())}</span>`});
                 break;
             case TokenType.ERROR_STR:
-                chunks.push({ type: 'TEXT', html: `<span class="token-error-string" data-title="${tokenDetails}">${htmlEscape(lexer.source.slice(lexer.tokenStart - 1, lexer.tokenEnd))}</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-error-string" data-title="${hint}">${htmlEscape(lexer.source.slice(lexer.tokenStart - 1, lexer.tokenEnd))}</span>`});
                 break;
             case TokenType.TEXT_BLOCK:
-                chunks.push({ type: 'TEXT', html: `<span class="token-string" data-title="${tokenDetails}">${htmlEscape(lexer.source.slice(lexer.tokenStart - 1, lexer.tokenEnd + 1))}</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-string" data-title="${hint}">${htmlEscape(lexer.source.slice(lexer.tokenStart - 1, lexer.tokenEnd + 1))}</span>`});
                 break;
             case TokenType.NUMBER:
-                chunks.push({ type: 'TEXT', html: `<span class="token-number" data-title="${tokenDetails}">${lexer.stringValue()}</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-number" data-title="${hint}">${lexer.stringValue()}</span>`});
                 break;
             case TokenType.COMPLEX_NUMBER:
-                chunks.push({ type: 'TEXT', html: `<span class="token-complex-number" data-title="${tokenDetails}">${lexer.stringValue()}</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-complex-number" data-title="${hint}">${lexer.stringValue()}</span>`});
                 break;
             case TokenType.VARIABLE:
-                chunks.push({ type: 'TEXT', html: `<span class="token-varable" data-title="${tokenDetails}">${lexer.stringValue()}</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-varable" data-title="${hint}">${lexer.stringValue()}</span>`});
                 break;
             case TokenType.LT:       // <
-                chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${tokenDetails}">&lt;</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${hint}">&lt;</span>`});
                 break;
             case TokenType.GT:       // >
-                chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${tokenDetails}">&gt;</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${hint}">&gt;</span>`});
                 break;
             case TokenType.LTE:      // <=
-                chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${tokenDetails}">&lt;=</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${hint}">&lt;=</span>`});
                 break;
             case TokenType.GTE:      // >=
-                chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${tokenDetails}">&gt;=</span>`});
+                chunks.push({ type: 'TEXT', html: `<span class="token-rw-characters" data-title="${hint}">&gt;=</span>`});
                 break;
             default: throw new Error(`[HighlightLerxer]: неизвестный токен ${token}`);
         }
         token = lexer.next();
-        tokenDetails =  TokenDetails[token].hint || '';
+        hint =  TokenDetails[token].hint || '';
     }
     // ПОСТ-ОБРАБОТКА:
     // Если в стеке остались открывающие скобки, у которых нет закрывающих,
