35b099f192d9a5a5de54852cd02d91470aa3caf4
moskvin73 <moskvin73av@gmail.com>
2026-09-03 22:50:48 +0300

    1

--- js/math_parser/MathParser.js
+++ js/math_parser/MathParser.js
@@ -444,7 +444,7 @@ export class MathParser extends EventTarget {
   async #parseCode(code) {
     let counter = 0;
     while (this.c_token !== TokenType.EOF) {
-     if (counter++ % 50 === 0) {
+     if (counter++ % 5 === 0) {
         await new Promise(resolve => setTimeout(resolve, 3500)); 
         this.dispatchEvent(new CustomEvent('parse-progress', { 
           detail: { message: `Парсинг обработано ${counter}% кода ` } 
