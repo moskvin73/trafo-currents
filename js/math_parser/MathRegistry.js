a54bf022c5518c12feae66fca222ca1b421f4c43
moskvin73 <moskvin73av@gmail.com>
2026-09-05 23:05:55 +0300

    1

--- js/math_parser/MathRegistry.js
+++ js/math_parser/MathRegistry.js
@@ -505,10 +505,20 @@ export const COMPILER_REGISTRY = new Map([
     { types: [RealNumber], callType: 'instance', method: 'log10' },
     { types: [ComplexNumber], callType: 'instance', method: 'log10' }
   ]}],
-  ['log', [
+  ['log', {
+    description: 'Возвращает значение математического выражения $\\mathtt{log(x, base)} \\implies \\log_{base}(x) = \\frac{\\ln(x)}{\\ln(base)}$.'+
+                 ' Функция полностью поддерживает комплексные числа.'+
+                 '<br><b>Поведение при особых значениях</b>'+
+                 '<ul>'+
+                 '<li>Отрицательный аргумент ($x < 0$): возвращает комплексное число с мнимой частью (так как вещественный логарифм отрицательного числа не определен).</li>'+
+                 '<li>Аргумент равен нулю ($x = 0$): возвращает $-\\infty$ (минус бесконечность).<li>'+
+                 '<li>Аргумент и основание равены нулю ($x = 0, base = 0$): возвращает \\color{red}\\text{NaN}.<li>'+
+                 '<li>Основание равно нулю ($base = 0$): возвращает $-j0$ (мнимый ноль с отрицательным знаком)<li>'+
+                 '<br>Тип данных аргумента <code>real, complex</code>.',
+    overloads: [
     { types: [RealNumber, RealNumber], callType: 'instance', method: 'logBase' },
     { types: [ComplexNumber, ComplexNumber], callType: 'instance', method: 'logBase' }
-  ]],
+  ]}],
 
   // === СТЕПЕНЬ ===
   ['pow', [
