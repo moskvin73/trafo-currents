6c6e1a87dee64688cb673cda7ed5f1df3ec7edef
moskvin73 <moskvin73av@gmail.com>
2026-09-13 01:52:00 +0300

    1

--- js/math_parser/ASTNodes.js
+++ js/math_parser/ASTNodes.js
@@ -233,50 +233,6 @@ export class StatementNode {
 }
 regAST(StatementNode);
 
-/*export class VarableCode {
-  // На этапе парсинга передаем statements и число — сколько у функции аргументов
-  constructor(statements, paramsCount, localsCount, lexicalParentFrame) {
-    this.statements = statements;
-    this.paramsCount = paramsCount;
-    this.localsCount = localsCount;
-    this.lexicalParentFrame = lexicalParentFrame; 
-  }
-
-  toJSON() {
-    if (this.lexicalParentFrame != null)
-      throw new Error(`Попытка сохранить не корнивойю процедуру.`);
-    return {
-      statements: this.statements,
-      paramsCount: this.paramsCount,
-      localsCount: this.localsCount,
-    };
-  }
-
-  static get dataTypeName() { return "VarableCode"; }
-
-  static fromJSON(data) {
-    const restoredstatements = data.statements.map(stm => restoreDataType(stm));
-    return new VarableCode(
-      restoredstatements,
-      data.paramsCount,
-      data.localsCount,
-      null
-    );
-  }
-
-  toRawTeX(settings) { return "\\text{code}"; }
-
-  evaluate(context, args) {
-    const scopeCtrl = context.scope_context;
-    const frame = scopeCtrl.createFrame(this.localsCount, this.lexicalParentFrame);
-    for (let i = 0; i < this.paramsCount; i++) {
-        frame.symbols[i].value = args[i];
-    }
-    scopeCtrl.scopes.push(frame);
-    return context.call_code(this.statements);
-  }
-}
-regAST(VarableCode);*/
 
 export class ErrorNode extends ASTNode {
   constructor(msg, loc) {
