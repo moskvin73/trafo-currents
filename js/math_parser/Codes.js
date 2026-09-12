365a57c515034de47ee10821a9bfaa778e1af3e3
moskvin73 <moskvin73av@gmail.com>
2026-09-13 02:02:01 +0300

    1

---
+++ js/math_parser/Codes.js
@@ -0,0 +1,27 @@
+export class Code {
+    constructor(loc = null, astNode = null) {
+        this.loc = loc;
+        this.astNode = astNode;
+    }
+
+    error(context, msg, loc) {
+        context.error(msg, loc ?? this.loc, "Runtime");
+    }
+
+    evaluate(context) {
+        try
+        {
+            return this.internal_evaluate(context);
+        }
+        catch(err)
+        {
+        this.error(context, err.toString());
+        return this.errorValue();
+        }
+    }
+ 
+    /** Внутренний метод вычисляет значение узла, возвращая экземпляр MathType (ComplexNumber/Matrix) */
+    internal_evaluate(context) {
+        throw new Error("[Code]: Метод evaluate() не реализован.");
+    }    
+}
