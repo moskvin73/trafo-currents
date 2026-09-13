e25b8d8b379a07b931bc4bd82297d48c8257d6d5
moskvin73 <moskvin73av@gmail.com>
2026-09-13 07:06:46 +0300

    1

--- js/math_parser/SymbolTableContext.js
+++ js/math_parser/SymbolTableContext.js
@@ -80,7 +80,8 @@ export class SymbolTableContext {
           throw new Error(`Идентификатор "${name}" является зарезервированным.`);
         },
         get description() { return description; },
-        get context { return instance; }
+        get context { return instance; },
+        get name { return name; }
       };
 
       this.fixedHash[name] = i; // Связываем имя с числовым ID
