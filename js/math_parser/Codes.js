3ab0c76eb4fedf9cb4df788c756dbadef22abc28
moskvin73 <moskvin73av@gmail.com>
2026-09-13 05:57:55 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -286,7 +286,15 @@ SubstitutionTableBin = new Map([
     ],
     [
         getBinKey(OperationCode.ADD, OperandsType.VARABLE, OperandsType.CONST),
-        ([l_o, r_o, loc, astNode]) => { const { l, r } = dispatcher.promoteTypes(l_op, r_o); return new OpConst(l.add(r)); }
+        ([l_o, r_o, loc, astNode]) => {return [new AddCodeValueValue(l_o, r_o, loc, astNode)]; }
+    ],
+    [
+        getBinKey(OperationCode.ADD, OperandsType.CONST, OperandsType.VARABLE),
+        ([l_o, r_o, loc, astNode]) => {return [new AddCodeValueValue(l_o, r_o, loc, astNode)]; }
+    ],
+    [
+        getBinKey(OperationCode.ADD, OperandsType.VARABLE, OperandsType.VARABLE),
+        ([l_o, r_o, loc, astNode]) => {return [new AddCodeValueValue(l_o, r_o, loc, astNode)]; }
     ],
     [
         getBinKey(OperationCode.ADD, OperandsType.EVALUATE, OperandsType.VALUE),
