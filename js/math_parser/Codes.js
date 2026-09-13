4df2e3af2dac200cc36d156f2ceabf30537cdf00
moskvin73 <moskvin73av@gmail.com>
2026-09-13 06:41:07 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -225,7 +225,27 @@ class BinCodeValueValue extends BaseBinCode {
     internal_evaluate(context) {
         const { l, r } = dispatcher.promoteTypes(this.l_value.getValue(context), this.r_value.getValue(context));
         stack.push(operator(l, r));
-    }    
+    }
+   
+  toJSON() {
+    return {
+      ...super.toJSON(),
+      l_value: this.l_value,
+      r_value: this.r_value,
+    };
+  }
+
+  static get dataTypeName() { return "BinCodeValueValue"; }
+
+  static fromJSON(data) {
+    return new BinCodeValueValue(
+      restoreDataType(data.l_value),
+      restoreDataType(data.r_value),
+      restoreLocation(data.loc),
+      restoreDataType(data.astNode)
+    );
+  }
+    
 }
 
 class BinCodeOpValue extends BaseBinCode {
