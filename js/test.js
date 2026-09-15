6592c147a301b2c7fb2d1519430090cf13160e47
moskvin73 <moskvin73av@gmail.com>
2026-09-16 00:05:06 +0300

    1

--- js/test.js
+++ js/test.js
@@ -78,3 +78,48 @@ export function test(data) {
     console.error("Критическая ошибка в тесте:", error);
   }    
 }
+
+export function test2(data) {
+    console.log("=== Старт тестирования сериализации ===");
+
+    console.log("Зарегистрированные типы:", debugRegistry());
+
+  try {
+
+    // Имитируем работу сессии №1: Пользователь создал глобальную переменную и записал туда матрицу
+    const context = new SymbolTableContext();
+    const id = context.acquireId("myCode");
+
+    // Допустим, интерпретатор записал в эту переменную вашу новую рабочую матрицу
+    const codes = [];
+    context.varSymbols[id - context.CD].value = new VarableCode(codes, 0, 0, null);
+
+    const savedState = context.serializeGlobalContext();
+    localStorage.setItem("global_symbol_table", savedState);
+    // --- ПЕРЕЗАГРУЗКА СТРАНИЦЫ (Сессия №2) ---
+
+    // Создаем абсолютно чистый контекст
+    const newContext = new SymbolTableContext();
+
+    // Загружаем состояние из LocalStorage
+    const storedState = localStorage.getItem("global_symbol_table");
+    newContext.deserializeGlobalContext(storedState);
+
+    // ПРОВЕРКА №1: Проверяем, что хэш-таблица имен восстановилась
+    const restoredId = newContext.acquireId("myCode"); 
+    console.log("ID совпадает со старым?", restoredId === id); // true
+
+    // ПРОВЕРКА №2: Проверяем, что матрица внутри переменной ожила со всеми методами
+    const variableIndex = restoredId - newContext.CD;
+    const codeInstance = newContext.varSymbols[variableIndex].value;
+    //console.log("Это экземпляр Matrix?", matrixInstance instanceof Matrix); // true
+    //console.log("ТеХ матрицы работает?", matrixInstance.toTeX()); // Выведет TeX вашей матрицы
+
+    // ПРОВЕРКА №3: Проверяем, что реактивность (сеттер) не сломалась
+    newContext.varSymbols[variableIndex].value = 42; // пишем примитив
+    console.log("Тип автоматически сменился на SYM_VARIABLE?", newContext.varSymbols[variableIndex].type);
+    
+  } catch (error) {
+    console.error("Критическая ошибка в тесте:", error);
+  }    
+}
