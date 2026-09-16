76fc83844e43ffd9a48497a376496115a9f88534
moskvin73 <moskvin73av@gmail.com>
2026-09-17 00:47:52 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -395,7 +395,7 @@ export class OpConst extends OpValue {
         this.value = value;
     }
 
-    getValue(context) { return value; }
+    getValue(context) { return this.value; }
 
     createCodePush() { return new PushCodeConst(this.value); }
 
