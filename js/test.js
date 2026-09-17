7135f9f82f90deb3bf09b5d7e4cd8353de5f5363
moskvin73 <moskvin73av@gmail.com>
2026-09-17 05:43:19 +0300

    1

--- js/test.js
+++ js/test.js
@@ -166,6 +166,8 @@ export function test3() {
                 console.log(` st[top] = ${this.evaluate_stack[len - 1]}`);
               c_le = len;
             }
+          } catch(err) {
+            throw err;
           } finally {
             console.log('***end evaluate***');
             this.is_evaluate = false;
