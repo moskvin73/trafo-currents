30bee0789b356b11eeaf82baa847972b5ef30591
moskvin73 <moskvin73av@gmail.com>
2026-09-18 01:59:31 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -1444,7 +1444,7 @@ export class CommandBuilder {
             this.#currentCode = 
                 this.#checkCountStackCommand(reateBinCode(operand, l, this.#currentCode), l_sc);
 		} else {
-            this.#append(this.#checkCountStackCommand(createBinCode(operand, l, r), l_sc + r_sc));
+            this.#append(this.#checkCountStackCommand(reateBinCode(operand, l, r), l_sc + r_sc));
 		}
 		return this;
 	}
