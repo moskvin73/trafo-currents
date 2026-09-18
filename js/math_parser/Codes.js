82902f8bfe4dc6093cecb4ad185e53bf512b5770
moskvin73 <moskvin73av@gmail.com>
2026-09-18 03:00:50 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -75,7 +75,7 @@ export class Command {
 
     get popStackCount() { throw new Error("[Command]: Геттер popStackCount не реализован."); }
 
-    get modifiesStack() { this.pushStackCount > 0 || this.popStackCount > 0; }
+    get modifiesStack() { return this.pushStackCount > 0 || this.popStackCount > 0; }
 
     toJSON() {
         return {
