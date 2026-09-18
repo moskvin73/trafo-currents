2450b8f07cd5ebef49f7ec4d5266778aa95846d8
moskvin73 <moskvin73av@gmail.com>
2026-09-18 15:24:39 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -1771,7 +1771,7 @@ const SubstitutionTableBin = new Map([
 ]);
 
 export function is_comands(comands) {
-    return Array.isArray(comands) && (comands.every(item => item instanceof Command));
+    return Array.isArray(comands) && comands.every(item => item instanceof Command);
 }
 
 function getOperandType(op) {
