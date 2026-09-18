d0a6f3743b25dee2c82caf7f826c24f1c1e3fc40
moskvin73 <moskvin73av@gmail.com>
2026-09-18 04:27:05 +0300

    1

--- js/test.js
+++ js/test.js
@@ -252,7 +252,7 @@ export function test3() {
   const command = builder
   .assign(op_n("pi"), op_c(Math.PI))
   .pop()
-  .add(op_c(10), op_c(1))       // 10 + 1 = 11
+  .sub(op_c(10), op_c(1))       // 10 + 1 = 11
   .sub(Code.self, op_c(-100))   // 10 + 1 + -100 = 89
   .neg(Code.self)
   .add(op_n("pi"), Code.self)   // pi + 89
