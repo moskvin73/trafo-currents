1e26a09c04e56fde441ad46a048ddaa47683c84c
moskvin73 <moskvin73av@gmail.com>
2026-09-18 15:21:58 +0300

    1

--- js/test.js
+++ js/test.js
@@ -253,7 +253,7 @@ export function test3() {
   const command = builder
   .sub(op_c(10), op_c(1))       // 10 + 1 = 11
   .sub(Code.self, op_c(-100))   // 10 + 1 + -100 = 89
-  .neg(Code.self);
+  .neg(Code.self)
   .assign(op_n("pi"), op_c(Math.PI))
   .pop()
   .sub(op_c(10), op_c(1))       // 10 + 1 = 11
