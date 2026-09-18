8a51967e57ff1e1f861dbc4c9a4ebb2a81eb35b7
moskvin73 <moskvin73av@gmail.com>
2026-09-18 11:37:00 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -77,6 +77,10 @@ export class Command {
 
     get modifiesStack() { return this.pushStackCount > 0 || this.popStackCount > 0; }
 
+    foldConstants(optimizedCode, simulatedStack) {
+        optimizedCode.push(this);
+    }
+
     toJSON() {
         return {
             dataType: this.constructor.dataTypeName,
