5ce8e4297278caddab581388c365f42f905b3d90
moskvin73 <moskvin73av@gmail.com>
2026-09-19 10:40:28 +0300

    1

--- js/test.js
+++ js/test.js
@@ -230,6 +230,10 @@ export function test3() {
         }
       },
 
+      error(message, loc, severity = 'error') {
+        console.log(`Ошибка: ${loc} ${severity}: ${message}`);
+      }
+
       toStringCommands() {
         if (!this.commands) return '';
         const ctx = this.scope_context;
