bc534dab23405981708535036e2ff89c96b1d7de
moskvin73 <moskvin73av@gmail.com>
2026-09-19 08:03:55 +0300

    1

--- js/test.js
+++ js/test.js
@@ -254,6 +254,7 @@ export function test3() {
   .sub(op_c(10), op_c(1))       // 10 + 1 = 11
   .sub(Code.self, op_c(-100))   // 10 + 1 + -100 = 89
   .neg(Code.self)
+  .pop()
   .assign(op_n("pi"), op_c(Math.PI))
   .pop()
   .sub(op_c(10), op_c(1))       // 10 + 1 = 11
