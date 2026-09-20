45598d869ebebe63cd8ac1c37a2504f88258dd74
moskvin73 <moskvin73av@gmail.com>
2026-09-20 11:38:36 +0300

    1

--- js/math_parser/SemanticDispatcher.js
+++ js/math_parser/SemanticDispatcher.js
@@ -226,8 +226,6 @@ export default class SemanticDispatcher {
     }
 
     const currentName = getTypeNameString(currentTypeId);
-    //const currentName = typeof currentTypeId === 'function' ? currentTypeId.name : currentTypeId;
-    //const targetName = typeof targetTypeId === 'function' ? targetTypeId.name : targetTypeId;
     const targetName = getTypeNameString(targetTypeId);
     throw new Error(`[Semantic Error]: Невозможно автоматически привести тип ${currentName} к ${targetName}`);
   }
