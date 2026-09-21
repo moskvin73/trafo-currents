f2dcc10d49c4f8571807819b8848d0b8ca5a414b
moskvin73 <moskvin73av@gmail.com>
2026-09-21 04:26:02 +0300

    1

--- js/test.js
+++ js/test.js
@@ -209,7 +209,7 @@ export function test3() {
 
             if (this.DEBUG) {
               const current_len = stack.length;
-              if (current_len > prev_stack_len || com.modifiesStack)
+              if (current_len > prev_stack_len)// || com.modifiesStack)
                 console.log(` st[top] = ${stack[current_len - 1]}`);
               prev_stack_len = current_len;
             }
