260519166e6f57d881ea633c52ac211b87b131b7
moskvin73 <moskvin73av@gmail.com>
2026-09-21 05:41:25 +0300

    1

--- js/math_parser/SemanticDispatcher.js
+++ js/math_parser/SemanticDispatcher.js
@@ -235,9 +235,9 @@ export const dispatcher = new SemanticDispatcher();
 
 export function toParserBase(value) {
     const type = typeof value;
-    const sourceType = type === 'object' && value !== null ? valueToCast.constructor : type;
+    const sourceType = type === 'object' && value !== null ? value.constructor : type;
 
-    if (!TYPE_REGISTRY.has(actualType)) throw new TypeError(`Недопустимый тип данных ${sourceType}`);
+    if (!TYPE_REGISTRY.has(sourceType)) throw new TypeError(`Недопустимый тип данных ${sourceType}`);
     const actualConfig = TYPE_REGISTRY.get(sourceType);
     if (actualConfig.selfPromote !== null) {
         return actualConfig.selfPromote(value);
