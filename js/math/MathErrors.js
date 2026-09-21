95f25031f0a7973d90d26eaa7ea56c05da725965
moskvin73 <moskvin73av@gmail.com>
2026-09-21 06:08:01 +0300

    1

--- js/math/MathErrors.js
+++ js/math/MathErrors.js
@@ -1,3 +1,17 @@
+export class ErrorMath extends TypeError {
+    constructor(message) {
+        super(message);
+        this.currentClassName = currentClassName;
+        this.name = "ErrorMath";
+    }
+
+    createMessages(funcGetTypeNameString) {
+        if (funcGetTypeNameString === 'function')
+            return interanlcreateMessages(funcGetTypeNameString); 
+        return ''; 
+    }
+}
+
 export class ErrorCastMathTypeNoValue extends TypeError {
     constructor(message, currentClassName) {
         super(message);
