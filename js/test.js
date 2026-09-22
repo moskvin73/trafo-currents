47d2fb9b8dc8e416a3a18b26a9c748cf2397407e
moskvin73 <moskvin73av@gmail.com>
2026-09-22 10:36:36 +0300

    1

--- js/test.js
+++ js/test.js
@@ -345,8 +345,10 @@ export function test3() {
 
   const contextError = {
     errors: [],
-    error(message, loc, severity = 'error') {
-      console.log(`[${severity}]: ${loc ?? 'Unknown location'} ${message}`);
+    error(message, loc, err, severity = 'error') {
+      let str_ref = getFirstStackTraceLinkRef(err);
+      if (str_ref && str_ref.length > 0) str_ref = '\n' + str_ref;
+      console.log(`[${severity}]: ${loc ?? 'Unknown location'} ${message}${str_ref}`);
     },
   };
 
