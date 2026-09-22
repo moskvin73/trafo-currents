292b9d1ed80210e7e99c306e0d37e0b899b48ca7
moskvin73 <moskvin73av@gmail.com>
2026-09-22 17:10:53 +0300

    1

--- js/test.js
+++ js/test.js
@@ -368,6 +368,7 @@ export function test3() {
   const command = builder 
   .push(15)
   .assign(op_n("z"), Code.self)
+  .pop()
   .sub(m, new Code.CommandBuilder().location(incLoc(c_loc)).push_gsym(acquireVar("z")))
   .location(incLoc(c_loc))
   .sub(Code.self, op_c(-100))
