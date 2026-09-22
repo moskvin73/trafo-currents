bfa6778d6e7bb90c050368352dfe4013ba7285c3
moskvin73 <moskvin73av@gmail.com>
2026-09-22 14:20:50 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -544,10 +544,10 @@ class TopStackToValueLoc extends Command {
     foldConstants(optimizedCode, simulatedStack) {
         const st_top = simulatedStack.pop();
         if (st_top.type === 'const') {
-            if (st_top instanceof ValueLoc)
-                simulatedStack.push({type: 'const', value: st_top});
+            if (st_top.value instanceof ValueLoc)
+                simulatedStack.push({type: 'const', value: st_top.value});
             else    
-                simulatedStack.push({type: 'const', value: new ValueLoc(st_top, this.loc)});
+                simulatedStack.push({type: 'const', value: new ValueLoc(st_top.value, this.loc)});
         } else {
             simulatedStack.push({ type: 'unknown' });
             optimizedCode.push(this);
