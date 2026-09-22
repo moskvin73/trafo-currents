05f75628ab61b8bd6b1c431c06e1a803ab173557
moskvin73 <moskvin73av@gmail.com>
2026-09-22 12:18:30 +0300

    2

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -45,10 +45,10 @@ export class Command {
         }
         catch(err)
         {
-            if (err instanceof ErrorBase)
-                throw new EvaluateError(err.message || String(err), context.evaluate_loc, { cause: err });
-            else if (err instanceof EvaluateError)
+            if (err instanceof EvaluateError)
                 throw new EvaluateError(err.message || String(err), err.location ?? context.evaluate_loc, { cause: err });
+            else if (err instanceof ErrorBase)
+                throw new EvaluateError(err.message || String(err), context.evaluate_loc, { cause: err });
             throw err;
         }
     }
