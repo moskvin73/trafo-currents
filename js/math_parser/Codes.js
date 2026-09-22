70029e9e6c85b7309c9250413f4201d032b28836
moskvin73 <moskvin73av@gmail.com>
2026-09-22 12:31:45 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -524,7 +524,7 @@ class PushCommConstLoc extends PushCommConst {
         this.loc = loc;
     }
 
-    commandName() { return 'push_loc'; }
+    commandName() { return `push ${this.loc},`; }
 
     internal_evaluate(context) {
         context.evaluate_stack.push(new ValueLoc(this.value, this.loc));
@@ -617,7 +617,7 @@ class PushCommVarbleLocalLoc extends PushCommVarbleLocal {
         this.loc = loc;
     }
 
-    commandName() { return 'push_loc'; }
+    commandName() { return `push ${this.loc},`; }
 
     internal_evaluate(context) {
         sym = context.scope_context.getSymbolById(this.id_name);
@@ -695,7 +695,7 @@ class PushCommVarbleGlobalLoc extends PushComm {
         this.loc = loc;
     }
 
-    commandName() { return 'push_loc'; }
+    commandName() { return `push ${this.loc},`; }
 
     internal_evaluate(context) {
         checkSymbol(this.symbol);
