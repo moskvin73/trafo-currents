90b7db00cb1752ec09292d24bd0c1653ad9b3629
moskvin73 <moskvin73av@gmail.com>
2026-09-25 20:41:58 +0300

    a

--- js/test.js
+++ js/test.js
@@ -345,7 +345,7 @@ export function test3() {
   const source_code = `
     A = (C = 10) - 11;
     B = [1, +2, -3];
-    A = A - (10 - B);
+    A = A - (10 - -B);
     `;
   const builder = parse(source_code);
 
