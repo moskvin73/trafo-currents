862889b9816739da582f9e5f457ce20d227528c7
moskvin73 <moskvin73av@gmail.com>
2026-09-25 21:26:56 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -1645,9 +1645,7 @@ class AssignCommValueConst extends Command {
     foldConstants(optimizedCode, simulatedStack, known_constants) {
         simulatedStack.push({type: 'const', value: this.value, index: optimizedCode.length});
         const old_v = this.value.get_constant_value(known_constants);    
-        if (old_v) {
-            optimizedCode[old_v.index].del = true;
-        }
+        if (old_v) optimizedCode[old_v.index].del = true;
         this.let_value.add_constant_value(known_constants, { value: this.value, index: optimizedCode.length });
         optimizedCode.push({ comm: this, del: false });     
     }
