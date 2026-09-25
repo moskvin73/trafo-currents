983e77694e139ae69749c661e8a80eb7eb6eb54e
moskvin73 <moskvin73av@gmail.com>
2026-09-25 12:17:45 +0300

    1

--- js/math_parser/Codes.js
+++ js/math_parser/Codes.js
@@ -1356,6 +1356,7 @@ class UnCommOp extends BaseUnComm {
             const calc_v = this.operator(toParserBase(st_top.value));
             simulatedStack.push({type: 'const', value: calc_v, index: optimizedCode.length});
             optimizedCode.push({ comm: new PushCommConst(calc_v), del: true });
+        }
         if (st_top.type === 'varable') {
             simulatedStack.push({ type: 'unknown' });
             optimizedCode[st_top.index].del = true;
@@ -1593,6 +1594,11 @@ class BinCommOpValue extends BaseBinComm {
             // Нужно перессоздать BinCommValueValue
             simulatedStack.push({ type: 'unknown' });
             optimizedCode.push({comm: this.recreateCommValueValue(new OpConst(st_top.value), this.value), del: false});
+        }
+        else if (st_top.type === 'varable') {
+            simulatedStack.push({ type: 'unknown' });
+            optimizedCode[st_top.index].del = true;    
+            optimizedCode.push({comm: this.recreateCommValueValue(st_top.value, this.value), del: false});
         } else {
             simulatedStack.push({ type: 'unknown' });
             optimizedCode.push({ comm: this, del: false });
